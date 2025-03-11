1eda229745efdaeb05f263408d668f02becb5bdc
Sefer Çınar <sefercinar18@hotmail.com>
2025-03-12 00:39:40 +0300

    analytics added

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,14 @@
         <Navbar/>
         <NuxtPage />
         <Footer/> 
+        <Analytics/>
     </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue';
 import Footer from '~/components/Footer.vue';
+import { Analytics } from '@vercel/analytics/nuxt'
 export default {
     components: {
         Navbar,
